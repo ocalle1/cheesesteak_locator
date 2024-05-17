27daf2afea760d20d3717ae7caf046d0ada9f65c
oscar calle <ocalle374@gmail.com>
2024-05-17 14:45:53 -0400

    initial commit

---
+++ lib/login_and_signoff/login_input.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginInput extends StatelessWidget {
+  const LoginInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
