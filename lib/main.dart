1cf6a18e32cdce64f446e58bac9e84a9cf09becb
oscar calle <ocalle374@gmail.com>
2024-05-18 18:12:39 -0400

    Carousel Slider added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cheesesteak_locator/login_and_signoff/login_input.dart';
 import 'package:cheesesteak_locator/login_and_signoff/signoff.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() {
   runApp(const LoginPage());
@@ -17,20 +18,66 @@ class LoginPage extends StatelessWidget {
       home: Scaffold(
         body: Stack(
           children: [
-            // Container(
-            //   height: 140.0,
-            //   width: 50.0,
-            //   decoration: BoxDecoration(
-            //     image: DecorationImage(
-            //       fit: BoxFit.contain,
-            //       image: AssetImage(
-            //           'assets/images/7d498f42-2e04-46a1-904c-97fa32032942.jpg'),
-            //     ),
-            //   ),
+            Positioned.fill(
+              child: CarouselImages(),
+            ),
+
+            // LoginInput(),
+            // Positioned.fill(
+            //   child: LoginInput(),
             // ),
-            LoginInput(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CarouselImages extends StatelessWidget {
+  const CarouselImages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sliders"),
+      ),
+      body: ListView(
+        children: [
+          CarouselSlider(
+            items: [
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                        image: NetworkImage(
+                          'https://images.unsplash.com/photo-1665940482653-ab7d3164b2f5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                        ),
+                        fit: BoxFit.fitHeight)),
+              ),
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8.0),
+                    image: DecorationImage(
+                        image: NetworkImage(
+                          'https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNoZWVzZXN0ZWFrJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D',
+                        ),
+                        fit: BoxFit.fitHeight)),
+              )
+            ],
+            options: CarouselOptions(
+                height: 380,
+                enlargeCenterPage: true,
+                autoPlay: true,
+                aspectRatio: 16 / 9,
+                autoPlayCurve: Curves.fastOutSlowIn,
+                enableInfiniteScroll: true,
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                viewportFraction: 0.8),
+          ),
+        ],
       ),
     );
   }
