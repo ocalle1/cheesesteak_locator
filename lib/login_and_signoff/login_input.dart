d8c0e84b57c064b5c962c5ae49ea5e942c35a6d1
oscar calle <ocalle374@gmail.com>
2024-05-18 18:12:21 -0400

    took out background and login options for now. Setting up a web site with no login/signup. Added a Carousel slider on the home page.

--- lib/login_and_signoff/login_input.dart
+++ lib/login_and_signoff/login_input.dart
@@ -1,68 +1,97 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+
+//
 
 class LoginInput extends StatelessWidget {
   const LoginInput({Key? key});
 
+//!Change background image - Testing to see if I could implement properly
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        extendBodyBehindAppBar: true,
-        body: Stack(
-          children: [
-            BackGroundImage(),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Username TextField
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 32),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 16), // Add some space between fields
-                  // Password TextField
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 32),
-                    child: TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('test'), centerTitle: true, backgroundColor: Colors.red),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1665940482653-ab7d3164b2f5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+            fit: BoxFit.cover,
+          ),
+        ],
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return const MaterialApp(
+  //     home: Scaffold(
+  //       extendBodyBehindAppBar: true,
+  //       body: Stack(
+  //         children: [
+  //           BackGroundImage(),
+  //           Center(
+  //             child: Column(
+  //               mainAxisAlignment: MainAxisAlignment.center,
+  //               children: [
+  //                 // Username TextField
+  //                 Padding(
+  //                   padding: EdgeInsets.symmetric(horizontal: 32),
+  //                   child: TextField(
+  //                     obscureText: true,
+  //                     decoration: InputDecoration(
+  //                       labelText: 'Username',
+  //                     ),
+  //                   ),
+  //                 ),
+  //                 SizedBox(height: 16), // Add some space between fields
+  //                 // Password TextField
+  //                 Padding(
+  //                   padding: EdgeInsets.symmetric(horizontal: 32),
+  //                   child: TextField(
+  //                     obscureText: true,
+  //                     decoration: InputDecoration(
+  //                       labelText: 'Password',
+  //                     ),
+  //                   ),
+  //                 ),
+  //               ],
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   );
+  // }
 }
 
 class BackGroundImage extends StatelessWidget {
-  const BackGroundImage({Key? key});
+  const BackGroundImage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Opacity(
-      opacity: 0.5,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              'https://images.unsplash.com/photo-1602704002454-95a38ac0163f?q=80&w=975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            ),
-            fit: BoxFit.cover,
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(
+              'assets/images/7d498f42-2e04-46a1-904c-97fa32032942.jpg'),
+          fit: BoxFit.cover,
         ),
       ),
     );
   }
 }
+
+    // return CachedNetworkImage(
+    //   imageUrl:
+    //       'https://i.etsystatic.com/22350138/r/il/6569e0/5220356899/il_600x600.5220356899_koes.jpg',
+    //   placeholder: (context, url) => Center(child: CircularProgressIndicator()),
+    //   errorWidget: (context, url, error) => Center(child: Icon(Icons.error)),
+    //   fit: BoxFit.cover,
+    //   width: double.infinity,
+    //   height: double.infinity,
+    // );
+  
